ca19b886456e95f6bfab076834fe638c5d969aa4
man-fel <96535824+man-fel@users.noreply.github.com>
2024-05-08 21:30:58 +0300

    Second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+let WEATHER_API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY
 
 export default function Home() {
 
-  const [place, setPlace] = useState("");
+  const [place, setPlace] = useState("Kisumu");
+
+  const getWeatherData = async ()=> {
+
+    if(place && place.length>0){
+      try{
+        let url = '//https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${WEATHER_API_KEY}'
+      }
+      catch(err){}
+    }
+  }
+
+  useEffect(()=>{getWeatherData()},[])
   return (
     <div>
     <h1>Hello World</h1>
